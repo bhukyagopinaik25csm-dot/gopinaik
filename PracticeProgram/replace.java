2accb57414de751dd1cb93dff59796c5880fee83
gopi naik bhukya <bhukyagopinaik.25.csm@anits.edu.in>
2026-09-17 20:55:49 +0530

    Delete PracticeProgram/replace.java

--- PracticeProgram/replace.java
+++
@@ -1,7 +0,0 @@
-public class replace {
-    public static void main(String[] args) {
-        String name="varun tej";
-        System.out.println("replace="+name.replace("varun tej","gopinaik"));
-    }
-    
-}
