fb06d8565c2d0246a40ab31ac2c895442568ff65
gopi naik bhukya <bhukyagopinaik.25.csm@anits.edu.in>
2026-09-17 20:57:30 +0530

    Delete PracticeProgram/usingconcate.java

--- PracticeProgram/usingconcate.java
+++
@@ -1,8 +0,0 @@
-class usingconcate {
-    public static void main(String[] args) {
-        String a="gopinaik";
-        String b="Bhukya";
-        String c=a.concat(b);
-        System.out.println(c);
-    }
-}
