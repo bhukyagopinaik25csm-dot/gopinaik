ed1b453f6ba3adeb4a896ecf30add4c2e3c88de1
gopi naik bhukya <bhukyagopinaik.25.csm@anits.edu.in>
2026-09-17 22:16:56 +0530

    Add files via upload

---
+++ PracticeProgram/encapsulation/basic.java
@@ -0,0 +1,23 @@
+class Student {
+    private int age;       // private variable
+    private String name;   // private variable
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+public class basic {
+    
+}
