3f5b9945699c553686f892a88c773fa9c9d2ab77
gopi naik bhukya <bhukyagopinaik.25.csm@anits.edu.in>
2026-09-17 20:53:30 +0530

    Delete PracticeProgram/contains.java

--- PracticeProgram/contains.java
+++
@@ -1,6 +0,0 @@
-public class contains {
-    public static void main(String[] args) {
-        String name="Ramcharan";
-        System.out.println("contains ramcharan="+name.contains(name));
-    }
-}
