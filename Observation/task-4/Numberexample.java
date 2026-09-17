4c00c80fa3dd1bc3fcab2b8038be3bb7e5b04a38
gopi naik bhukya <bhukyagopinaik.25.csm@anits.edu.in>
2026-09-17 21:50:08 +0530

    Add files via upload

---
+++ Observation/task-4/Numberexample.java
@@ -0,0 +1,16 @@
+public class Numberexample {
+    
+    public static void main(String[] args) {
+        try {
+            String value = "ABC";
+
+            int number = Integer.parseInt(value);
+
+            System.out.println(number);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Invalid number format");
+        }
+    }
+
+}
